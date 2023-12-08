0aa53a6ffe2b668a5d05f9ce6cfac032a245e991
roymathew <roym5252@gmail.com>
2023-12-08 00:33:13 +0000

    Add splashscreen support for APIs below 12

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,6 +56,7 @@ dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
+    implementation (libs.androidx.core.splashscreen)
     implementation(platform("androidx.compose:compose-bom:2023.03.00"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
