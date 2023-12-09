7d4b5dfa44690e888dbab0e0cacff81015bb9f39
roymathew <roym5252@gmail.com>
2023-12-09 15:11:46 +0000

    Make `check screen title is shown` test pass

--- feature-product-list/src/main/java/com/jlp/feature_product_list/ui/ProductListScreen.kt
+++ feature-product-list/src/main/java/com/jlp/feature_product_list/ui/ProductListScreen.kt
@@ -10,32 +10,50 @@ import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.testTag
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.PreviewParameter
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.jlp.core.model.Product
 import com.jlp.core.ui.theme.CustomColor
 import com.jlp.feature_product_list.ui.sampledata.SampleProductProvider
 
 @Preview
 @Composable
-fun ProductListScreen(@PreviewParameter(SampleProductProvider::class) products:List<Product>){
-    
-    Box(modifier = Modifier
-        .background(color = CustomColor.Grey)
-        .fillMaxSize()){
+fun ProductListScreen(@PreviewParameter(SampleProductProvider::class) products: List<Product>) {
+
+    Box(
+        modifier = Modifier
+            .background(color = CustomColor.Grey)
+            .fillMaxSize()
+    ) {
 
         Column {
 
-            Text(text = "Dishwasher", modifier = Modifier.testTag("productListTitle"))
+            Text(
+                text = "Dishwasher (114)",
+                style = TextStyle(
+                    fontSize = 30.sp,
+                    fontWeight = FontWeight(800),
+                    color = Color(0xFF000000),
 
-            LazyVerticalGrid(columns = GridCells.Adaptive(200.dp), Modifier.testTag("productList"), content = {
+                    ),
+                modifier = Modifier.testTag("productListTitle")
+            )
 
-                items(products) { product ->
-                    ProductGridItem(product = product)
-                }
-            })
+            LazyVerticalGrid(
+                columns = GridCells.Adaptive(200.dp),
+                Modifier.testTag("productList"),
+                content = {
+
+                    items(products) { product ->
+                        ProductGridItem(product = product)
+                    }
+                })
         }
 
     }
