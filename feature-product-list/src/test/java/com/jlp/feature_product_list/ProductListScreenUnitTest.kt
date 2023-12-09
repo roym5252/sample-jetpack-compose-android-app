c0afb9cbd4a01d52816e403faeb79951d613db89
roymathew <roym5252@gmail.com>
2023-12-09 14:57:57 +0000

    -Move common code in test under Before annotation -Add failing test for showing title in product listing screen

--- feature-product-list/src/test/java/com/jlp/feature_product_list/ProductListScreenUnitTest.kt
+++ feature-product-list/src/test/java/com/jlp/feature_product_list/ProductListScreenUnitTest.kt
@@ -6,6 +6,7 @@ import androidx.compose.ui.test.onNodeWithTag
 import androidx.compose.ui.test.onNodeWithText
 import com.jlp.core.model.Product
 import com.jlp.feature_product_list.ui.ProductListScreen
+import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -19,48 +20,41 @@ class ProductListScreenUnitTest {
     @get:Rule
     val composeTestRule = createComposeRule()
 
+    @Before
+    fun setUp(){
+
+        composeTestRule.setContent {
+            ProductListScreen(listOf(Product("Product 1",null,"£100.00")))
+        }
+    }
+
+    @Test
+    fun `check screen title is shown`() {
+        composeTestRule.onNodeWithTag("productListTitle").assertIsDisplayed()
+    }
+
     @Test
     fun `check product grid is displayed`() {
-        // Start the app
-        composeTestRule.setContent {
-            ProductListScreen(listOf(Product("Product 1","","")))
-        }
         composeTestRule.onNodeWithTag("productList").assertIsDisplayed()
     }
 
     @Test
     fun `check product image is displayed on grid`() {
-        // Start the app
-        composeTestRule.setContent {
-            ProductListScreen(listOf(Product("Product 1",null,"")))
-        }
         composeTestRule.onNodeWithTag("productImage").assertIsDisplayed()
     }
 
     @Test
     fun `check product name is displayed on grid`() {
-        // Start the app
-        composeTestRule.setContent {
-            ProductListScreen(listOf(Product("Product 1",null,"")))
-        }
         composeTestRule.onNodeWithTag("productName").assertIsDisplayed()
     }
 
     @Test
     fun `check product price is displayed on grid`() {
-        // Start the app
-        composeTestRule.setContent {
-            ProductListScreen(listOf(Product("Product 1",null,"£100")))
-        }
         composeTestRule.onNodeWithTag("productPrice").assertIsDisplayed()
     }
 
     @Test
     fun `check product price is displaying with two decimal places on grid`() {
-        // Start the app
-        composeTestRule.setContent {
-            ProductListScreen(listOf(Product("Product 1",null,"£100.00")))
-        }
         composeTestRule.onNodeWithText("£100.00").assertIsDisplayed()
     }
 
