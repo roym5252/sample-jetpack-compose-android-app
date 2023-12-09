70a3c7bf2590217781c0aa5deb548dda3275ae1a
roymathew <roym5252@gmail.com>
2023-12-09 12:04:48 +0000

    -Make 'check product name is displayed on grid' test pass

--- feature-product-list/src/main/java/com/jlp/feature_product_list/ui/ProductGridItem.kt
+++ feature-product-list/src/main/java/com/jlp/feature_product_list/ui/ProductGridItem.kt
@@ -11,6 +11,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.sizeIn
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -61,6 +62,7 @@ fun ProductGridItem(product: Product) {
                 contentScale = ContentScale.Crop,
             )
 
+            Text(text = "Product Name", Modifier.testTag("productName"))
         }
     }
 }
