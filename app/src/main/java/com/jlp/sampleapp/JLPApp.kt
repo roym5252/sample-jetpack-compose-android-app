d9495123e822fb27ede69a9891433ef9c85b0ac4
roymathew <roym5252@gmail.com>
2023-12-17 23:44:40 +0000

    -Add emulator and rooted device check before loading API key.

--- app/src/main/java/com/jlp/sampleapp/JLPApp.kt
+++ app/src/main/java/com/jlp/sampleapp/JLPApp.kt
@@ -1,12 +1,14 @@
 package com.jlp.sampleapp
 
 import android.app.Application
+import com.jlp.core.util.CommonUtil
 import com.jlp.core.util.PrefUtil
+import com.scottyab.rootbeer.RootBeer
 import dagger.hilt.android.HiltAndroidApp
 import timber.log.Timber
 import timber.log.Timber.Forest.plant
 import javax.inject.Inject
-
+import kotlin.system.exitProcess
 
 @HiltAndroidApp
 class JLPApp: Application() {
@@ -21,13 +23,20 @@ class JLPApp: Application() {
             plant(Timber.DebugTree())
         }
 
-        /*if (CommonUtil().isEmulator()||RootBeer(this).isRooted){
-            exitProcess(0)
+        //Checking if build is release version.
+        if(BuildConfig.BUILD_TYPE.contentEquals("release")&&!BuildConfig.DEBUG){
+
+            //Checking if app is running on emulator or device is rooted
+            if (CommonUtil().isEmulator()|| RootBeer(this).isRooted){
+
+                //Exiting app
+                exitProcess(0)
+            }else{
+                prefUtil.saveString("api_key",BuildConfig.API_KEY)
+            }
+
         }else{
             prefUtil.saveString("api_key",BuildConfig.API_KEY)
-        }*/
-
-        prefUtil.saveString("api_key",BuildConfig.API_KEY)
-
+        }
     }
 }
